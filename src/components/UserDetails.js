5589ae24c34dfa022005b6fdf3b2ae8e8973d345
Ajayp111 <126410871+Ajayp111@users.noreply.github.com9>
2024-01-24 21:59:40 +0530

    fine

---
+++ src/components/UserDetails.js
@@ -0,0 +1,70 @@
+import { Spinner } from "react-bootstrap";
+
+const UserDetails = (props) => {
+  const { user, handleImageError, handleImageLoad, imageLoading } = props.a;
+
+  console.log(user);
+  return (
+    <section className="w-75 pr-5" style={{ marginRight: "20px" }}>
+      <h1
+        className="fs-4 m-0 p-3 text-center"
+        style={{ backgroundColor: "#F9E795" }}
+      >
+        User Details
+      </h1>
+      <div className="d-flex flex-column align-items-center">
+        {imageLoading ? (
+          <div className="img-circle2 mt-3 d-flex align-items-center justify-content-center">
+            <Spinner animation="border" variant="info" />
+          </div>
+        ) : (
+          <img
+            className="img-circle2 mt-2"
+            src={user?.avatar}
+            onError={handleImageError}
+            onLoad={handleImageLoad}
+            alt=""
+          />
+        )}
+
+        <p className="m-0 fs-6 pb-3">@{user?.profile?.username}</p>
+        <p className="m-0 p-2 border border-dark-subtle w-100 fs-5">
+          {user?.Bio}
+        </p>
+
+        <div className="w-100 mt-4">
+          <p className="text-start m-0 fs-5">Full Name</p>
+          <p
+            className="text-start m-0 p-2 border border-dark-subtle bg-opacity-10 fs-5"
+            style={{ backgroundColor: "#F96167" }}
+          >
+            <span>{user?.profile?.firstName}</span> {user?.profile?.lastName}
+          </p>
+        </div>
+
+        <div className="w-100 mt-3">
+          <p className="text-start m-0 fs-5">Job Title</p>
+          <p
+            className="text-start m-0 p-2 border border-dark-subtle bg-opacity-10 fs-5"
+            style={{ backgroundColor: "#F96167" }}
+          >
+            {" "}
+            {user?.jobTitle}
+          </p>
+        </div>
+
+        <div className="w-100 mt-3">
+          <p className="text-start m-0 fs-5">Email</p>
+          <p
+            className="text-start m-0 text-lowercase p-2 border border-dark-subtle bg-opacity-10 fs-5"
+            style={{ backgroundColor: "#F96167" }}
+          >
+            {user?.profile?.email}
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default UserDetails;
---
+++ src/components/Users.js
@@ -0,0 +1,131 @@
+import { useState, useEffect } from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+
+const AllUsers = ({ handleShowUserInfo, handleImageError }) => {
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const apiUrl = "https://602e7c2c4410730017c50b9d.mockapi.io/users";
+
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const validUsers = data.filter((user) => user?.avatar);
+
+        const filteredUsers = validUsers.slice(15);
+
+        setUsers(filteredUsers);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setError(error);
+      });
+  }, []);
+
+  if (error) {
+    return <div>Error fetching data: {error.message}</div>;
+  }
+
+  const responsive = {
+    0: { items: 1 },
+    600: { items: 2 },
+    1024: { items: 3 },
+  };
+
+  const arrowStyles = {
+    width: "60px",
+    height: "60px",
+  };
+
+  const dotStyles = {
+    width: "12px",
+    height: "12px",
+    marginLeft: "5px",
+    marginRight: "5px",
+  };
+
+  return (
+    <section className="w-100">
+      <h1
+        className="fs-4 mb-2 p-3 border text-center bg-opacity-10"
+        style={{ backgroundColor: "#F96167" }}
+      >
+        Users List
+      </h1>
+
+      <AliceCarousel
+        responsive={responsive}
+        mouseTracking
+        infinite
+        stagePadding={{ paddingLeft: 20, paddingRight: 20 }}
+        buttonsDisabled={false}
+        disableDotsControls={false}
+        renderPrevButton={({ isDisabled }) => (
+          <button
+            style={{
+              ...arrowStyles,
+              left: 0,
+              backgroundColor: "#F96167",
+              color: "black",
+            }}
+            disabled={isDisabled}
+          >
+            Prev
+          </button>
+        )}
+        renderNextButton={({ isDisabled }) => (
+          <button
+            style={{
+              ...arrowStyles,
+              right: 0,
+              backgroundColor: "#F9E795",
+              color: "white",
+            }}
+            disabled={isDisabled}
+          >
+            Next
+          </button>
+        )}
+        renderDotsItem={({ isActive }) => (
+          <span
+            style={{
+              ...dotStyles,
+              backgroundColor: isActive ? "#000" : "#999",
+            }}
+          />
+        )}
+      >
+        {users.map((user, index) => (
+          <div
+            key={user?.profile?.username}
+            // onClick={() => handleShowUserInfo(index)}
+            className="img-div p-2 border d-flex flex-column gap-3 align-items-center"
+          >
+            <img
+              className="img-circle"
+              src={user?.avatar}
+              onError={handleImageError}
+              alt=""
+            />
+            <div>
+              <p className="m-0 fs-5 fw-semibold">
+                {user?.profile?.firstName}{" "}
+                <span>{user?.profile?.lastName}</span>
+              </p>
+              <p>{user?.jobTitle}</p>
+            </div>
+          </div>
+        ))}
+      </AliceCarousel>
+    </section>
+  );
+};
+
+export default AllUsers;
